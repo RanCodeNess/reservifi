2f83472be3e15c93804862514de63d1dbb8be7b4
RanCodeNess <rancodeness@gmail.com>
2024-07-07 15:34:04 +0100

    "Updated HTML, CSS, and JavaScript files: modified navigation menu, added new elements to main container, and made various styling changes."

--- script.js
+++ script.js
@@ -2,17 +2,30 @@
 // Variables declaration
 //
 
-let toggleMenu = document.querySelector('.menu-toggle');
-console.log(toggleMenu)
+// let toggleMenu = document.querySelector('.menu-toggle');
+// console.log(toggleMenu)
 
-//
-// Functions declaration
-//
+// //
+// // Functions declaration
+// //
 
-//
-// Main code
-//
-toggleMenu.addEventListener('click',() =>{
-    document.querySelector('.nav-links').classList.toggle('show');
-    console.log('f')
-} )
+// //
+// // Main code
+// //
+// toggleMenu.addEventListener('click',() =>{
+//     document.querySelector('.nav-links').classList.toggle('show');
+//     console.log('f')
+// } )
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    if (navLinks.classList.contains('show')) {
+        navLinks.style.height = '0px';
+    } else {
+        navLinks.style.height = navLinks.scrollHeight + 'px';
+    }
+    navLinks.classList.toggle('show');
+});
+
+
